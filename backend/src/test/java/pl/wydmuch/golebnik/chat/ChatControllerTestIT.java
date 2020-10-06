9dd953868b18a6f327fffa59f301f869d00683a9
Patryk Wydmuch <pvydmuch@gmail.com>
2020-10-06 20:22:42 +0200

    Refactor chat integration test a bit

--- backend/src/test/java/pl/wydmuch/golebnik/chat/ChatControllerTestIT.java
+++ backend/src/test/java/pl/wydmuch/golebnik/chat/ChatControllerTestIT.java
@@ -25,22 +25,19 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChatControllerTestIT {
 
-    @LocalServerPort
-    private int port;
-    private String URL;
-
     private static final String SEND_MESSAGE_ENDPOINT = "/app/chat.sendMessage";
     private static final String ADD_USER_ENDPOINT = "/app/chat.addUser";
     private static final String SUBSCRIBE_PUBLIC = "/topic/public";
 
+    @LocalServerPort
+    private int port;
     private CompletableFuture<ChatMessage> completableFuture;
-
-    StompSession stompSession;
+    private StompSession stompSession;
 
     @BeforeEach
     public void setup() throws InterruptedException, ExecutionException, TimeoutException {
         completableFuture = new CompletableFuture<>();
-        URL = "ws://localhost:" + port + "/ws";
+        String URL = "ws://localhost:" + port + "/ws";
         WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
         stompSession = stompClient.connect(URL, new StompSessionHandlerAdapter() {}).get(10, SECONDS);
@@ -48,7 +45,6 @@ class ChatControllerTestIT {
 
     @Test
     public void message_shouldBeDeliveredToSubscriber_whenSent() throws InterruptedException, ExecutionException, TimeoutException {
-
         ChatMessage expectedMessage = new ChatMessage(ChatMessage.MessageType.CHAT,"hello","john");
 
         stompSession.subscribe(SUBSCRIBE_PUBLIC , new ChatMessageStompFrameHandler());
@@ -60,7 +56,6 @@ class ChatControllerTestIT {
 
     @Test
     public void testMakeMoveEndpoint() throws InterruptedException, ExecutionException, TimeoutException {
-
         String sender = "john";
         ChatMessage expectedMessage = new ChatMessage(ChatMessage.MessageType.CHAT,"hello", sender);
 
@@ -78,6 +73,7 @@ class ChatControllerTestIT {
     }
 
     private class ChatMessageStompFrameHandler implements StompFrameHandler {
+
         @Override
         public Type getPayloadType(StompHeaders stompHeaders) {
             System.out.println(stompHeaders.toString());
